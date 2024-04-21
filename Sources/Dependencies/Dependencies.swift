f7cabb0ce76ea074fc6c16840c9dfed7bbc637cd
Pedro <pedro@pepicrft.me>
2024-04-21 09:49:57 +0200

    Add build task

--- Sources/Dependencies/Dependencies.swift
+++ Sources/Dependencies/Dependencies.swift
@@ -1,36 +1,120 @@
 import Foundation
 import SwiftUI
 
-// struct ContextKey<T>: Hashable {
-//     let type: T.Type
-    
-//     init(_ type: T.Type) {
-//         self.type = type
-//     }
-//     static func == (lhs: ContextKey, rhs: ContextKey) -> Bool {
-//         return String(describing: lhs.type) == String(describing: rhs.type)
-//     }
-    
-//     func hash(into hasher: inout Hasher) {
-//         hasher.combine(String(describing: type))
-//     }
-// }
+public enum DependencyType {
+    case multi
+    case single
+}
 
-// actor Context {
-//     private var dependencies: [ContextKey<Any>: Any] = [:]
+private extension DependencyType {
+    func factory<T>(factory: @escaping (DependenciesResolver) async throws -> T) ->  DependencyFactory {
+        switch self {
+        case .multi:
+            return .multi(factory)
+        case .single:
+            return .single(factory)
+        }
+    }
+}
+
+enum DependencyFactory {
+    case single((DependenciesResolver) async throws -> Any)
+    case multi((DependenciesResolver) async throws -> Any)
+}
+
+public enum DependenciesError: Error, CustomStringConvertible {
+    case alreadyRegisteredDependency(Any.Type)
+    case dependencyNotFound(Any.Type)
+    case circularDependency([String])
     
-//     public init() {}
-  
-//     public func register<V: Actor>(_ dependency: V) {
-//         dependencies[ContextKey(V.self as! (any Any).Type)] = dependency
-//     }
+    public var description: String {
+        switch self {
+        case let .alreadyRegisteredDependency(type):
+            return "The dependency \(String(describing: type)) has already been registered"
+        case let .dependencyNotFound(type):
+            return "The dependency \(String(describing: type)) is not registered"
+        case let .circularDependency(dependencyChain):
+            return "We found a circular dependency: \(dependencyChain.map({ String(describing: $0) }).joined(separator: " > "))"
+        }
+    }
+}
+
+public actor DependenciesResolver {
+    let dependencies: Dependencies
+    let dependencyChain: [String]
     
-//     public subscript<V: Actor>(_ type: V.Type) -> V {
-//         let value = dependencies[ContextKey(V.self as! (any Any).Type)]
-//         if let castedValue = value as? V {
-//             return castedValue
-//         } else {
-//             fatalError("Nooo")
-//         }
-//     }
-// }
+    init(dependencies: Dependencies, dependencyChain: [String]) {
+        self.dependencies = dependencies
+        self.dependencyChain = dependencyChain
+    }
+    
+    public func resolve<T>(_ type: T.Type) async throws -> T {
+        var dependencyChain = dependencyChain
+        dependencyChain.append(String(describing: type))
+        if let _ = dependencyChain.anyDuplicate() {
+            throw DependenciesError.circularDependency(dependencyChain)
+        }
+        return try await self.dependencies.dependency(type, dependencyChain: dependencyChain)
+    }
+}
+
+public actor Dependencies {
+    
+    /**
+     This dictionary contains the factory functions to create an instance of a given dependency. The factory functions
+     are indexed by an object identifier obtained by the type of dependency.
+     The instantiation of the dependencies happens lazily when `dependency` is invoked, ensuring that the type (`.multi` or `.single`)
+     is respected.
+     */
+    private var factories: [ObjectIdentifier: DependencyFactory] = [:]
+    private var singleDependencies: [ObjectIdentifier: Any] = [:]
+    
+    public func register<T>(_ type: DependencyType = .single, _ factory: @escaping (DependenciesResolver) async throws -> T) throws {
+        let identifier = ObjectIdentifier(T.self)
+
+        if factories[identifier] != nil {
+            throw DependenciesError.alreadyRegisteredDependency(T.self)
+        }
+        
+        factories[identifier] = type.factory(factory: factory)
+    }
+    
+    public func dependency<T>(_ type: T.Type) async throws -> T {
+        try await dependency(T.self, dependencyChain: [])
+    }
+    
+    fileprivate func dependency<T>(_ type: T.Type, dependencyChain: [String]) async throws -> T {
+        let identifier = ObjectIdentifier(T.self)
+
+        guard let factory = factories[identifier] else {
+            throw DependenciesError.dependencyNotFound(T.self)
+        }
+        
+        let resolver = DependenciesResolver(dependencies: self, dependencyChain: dependencyChain)
+              
+        switch factory {
+        case let .multi(instantiate):
+            return try await instantiate(resolver) as! T
+        case let .single(instantiate):
+            if let singleDependency = singleDependencies[identifier] {
+                return singleDependency as! T
+            }
+            let singleDependency = try await instantiate(resolver)
+            singleDependencies[identifier] = singleDependency
+            return singleDependency as! T
+        }
+    }
+}
+
+private extension Array where Element: Hashable {
+    func anyDuplicate() -> Element? {
+        var seen = Set<Element>()
+        for value in self {
+            if seen.contains(value) {
+                return value
+            }
+            seen.insert(value)
+        }
+        return nil
+    }
+}
--- Tests/DependenciesTests/DependenciesTests.swift
+++ Tests/DependenciesTests/DependenciesTests.swift
@@ -4,14 +4,115 @@ import XCTest
 @testable import Dependencies
 
 final class ContextTests: XCTestCase {
-    actor ProcessRunner {}
-    
-    func test_register() async {
+    func test_register_throwsAnError_when_theSameDependencyIsRegisteredMoreThanOnce() async throws {
+        // Given
         let dependencies = Dependencies()
-        // let processRunner = ProcessRunner()
-        // await context.register(processRunner)
-        // let got = await context[ProcessRunner.self]
+        try await dependencies.register(.single) { _ in StructA()}
         
-        // XCTAssertIdentical(got, processRunner)
+        // When
+        var thrownError: DependenciesError?
+        do {
+            try await dependencies.register(.single) { _ in StructA() }
+        } catch {
+            thrownError = error as? DependenciesError
+        }
+        
+        // Then
+        XCTAssertNotNil(thrownError)
+        XCTAssertEqual(thrownError?.description, "The dependency StructA has already been registered")
+    }
+    
+    func test_register_throwsAnError_when_aCircularDependencyIsDetected() async throws {
+        // Given
+        let dependencies = Dependencies()
+        try await dependencies.register(.single) { resolver in
+            return CircularA(b: try await resolver.resolve(CircularB.self))
+        }
+        try await dependencies.register(.single) { resolver in
+            return CircularB(a: try await resolver.resolve(CircularA.self))
+        }
+        
+        // When
+        var thrownError: DependenciesError?
+        do {
+            _ = try await dependencies.dependency(CircularA.self)
+        } catch {
+            thrownError = error as? DependenciesError
+        }
+        
+        // Then
+        XCTAssertNotNil(thrownError)
+        XCTAssertEqual(thrownError?.description, "We found a circular dependency: CircularB > CircularA > CircularB")
+    }
+    
+    func test_dependency_throwsAnError_when_aDependencyIsNotRegistered() async throws {
+        // Given
+        let dependencies = Dependencies()
+        
+        // When
+        var thrownError: DependenciesError?
+        do {
+            _ = try await dependencies.dependency(ClassA.self)
+        } catch {
+            thrownError = error as? DependenciesError
+        }
+        
+        // Then
+        XCTAssertNotNil(thrownError)
+        XCTAssertEqual(thrownError?.description, "The dependency ClassA is not registered")
+    }
+    
+    func test_dependency_returnsTheSameInstance_whenSingle() async throws {
+        // Given
+        let dependencies = Dependencies()
+        try await dependencies.register(.single) { _ in ClassA()}
+        
+        // When
+        let first = try await dependencies.dependency(ClassA.self)
+        let second = try await dependencies.dependency(ClassA.self)
+
+        // Then
+        XCTAssertIdentical(first, second)
+    }
+    
+    func test_dependency_returnsMultipleInstances_whenMulti() async throws {
+        // Given
+        let dependencies = Dependencies()
+        try await dependencies.register(.multi) { _ in ClassA()}
+        
+        // When
+        let first = try await dependencies.dependency(ClassA.self)
+        let second = try await dependencies.dependency(ClassA.self)
+
+        // Then
+        XCTAssertNotIdentical(first, second)
     }
 }
+
+
+private struct StructA {}
+
+private struct StructB {
+    let a: StructA
+}
+
+private struct StructC {
+    let a: StructA
+    let b: StructB
+}
+
+private class CircularA {
+    var b: CircularB
+    init(b: CircularB) {
+        self.b = b
+    }
+}
+
+private class CircularB {
+    var a: CircularA
+    init(a: CircularA) {
+        self.a = a
+    }
+}
+
+private class ClassA {}
